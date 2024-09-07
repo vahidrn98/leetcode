546aa0cb68ff8d62e320db1c50ee2d8986f1f3a4
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-09-07 00:04:51 -0230

    StringGCD

--- IncreasingTripplet.py
+++ IncreasingTripplet.py
@@ -16,3 +16,21 @@ class Solution:
                             return True
         return False
         
+# optimized solution
+# class Solution:
+#     def increasingTriplet(self, nums: List[int]) -> bool:
+#         if len(nums) < 3:
+#             return False
+
+#         first = float('inf')
+#         second = float('inf')
+
+#         for num in nums:
+#             if num <= first:
+#                 first = num
+#             elif num <= second:
+#                 second = num
+#             else:
+#                 return True
+        
+#         return False
---
+++ StringGCD.py
@@ -0,0 +1,26 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        n1 = len(str1)
+        n2 = len(str2)
+
+        if(n1==0 or n2==0):
+            return ""
+
+        largest = ""
+        if(n1<=n2):
+            for i in range(n1):
+                c = str1[:i+1]
+                if(str1 == c*(n1//(i+1))):
+                    if(str2 == c*(n2//(i+1))):
+                        largest = str1[:i+1]
+        elif(n2<n1):
+            for i in range(n2):
+                c = str2[:i+1]
+               
+                if(str2 == c*(n2//(i+1))):
+                    if(str1 == c*(n1//(i+1))):
+                        largest = str2[:i+1]
+        return largest
+
+
+        
