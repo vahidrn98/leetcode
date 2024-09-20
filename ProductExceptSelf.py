2d6f5e3df8a6d7dbf9168925a7c56a7a64ae762d
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-09-19 23:30:02 -0230

    medium product except self

--- ProductExceptSelf.py
+++ ProductExceptSelf.py
@@ -2,28 +2,24 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
         answer = []
-        table = []
+        after = []
+        before = []
 
+        prod = 1
         for i in range(n):
-            table.append([])
-            for j in range(n):
-                table[i].append(None)
-        
+            prod *= nums[n-1-i]
+            after.append(prod)
+        after = after[::-1]
+
+        prod = 1
         for i in range(n):
-            for j in range(i,n):
-                if(table[i][j]==None):
-                    if(i==j):
-                        table[i][j] = nums[i]
-                    else:
-                        table[i][j] = table[i][j-1]*nums[j]
-        # print(table)
+            prod *= nums[i]
+            before.append(prod)
         for i in range(n):
             if(i==0):
-                # print(i+1)
-                # print(n-1)
-                answer.append(table[i+1][n-1])
+                answer.append(after[1])
             elif(i==n-1):
-                answer.append(table[0][i-1])
+                answer.append(before[n-2])
             else:
-                answer.append(table[0][i-1] * table[i+1][n-1])
+                answer.append(after[i+1]*before[i-1])
         return answer
