1e326028aef40513ff867742b41d0a35c5730874
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-09-21 12:32:07 -0230

    canjump new solution

--- CanJump.py
+++ CanJump.py
@@ -1,11 +1,16 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        gas = 0
-        for n in nums:
-            if gas < 0:
-                return False
-            elif n > gas:
-                gas = n
-            gas -= 1
-            
-        return True
+        
+        i = 0
+        s = [0]
+        while(len(s)>0):
+            i = s.pop()
+            if(i==len(nums)-1):
+                return True
+            if(nums[i]>0):
+                for a in range(1,nums[i]+1):
+                    if(i+a>=len(nums)-1):
+                        return True
+                    if(i+a not in s):
+                        s.append(i+a)
+        return False
