5d0f5b17e94fcf9514424f5456c5146ae14ea455
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-09-24 18:20:31 -0230

    K Closest with heap

--- KClosest.py
+++ KClosest.py
@@ -17,3 +17,25 @@ class Solution:
             i+=1
 
         return ans
+    
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        dist = []
+        heapq.heapify(dist)
+        ans = []
+
+        size = 0
+        for p in points:
+            heapq.heappush(dist,(-(p[0]**2+p[1]**2),p))
+            size+=1
+            if(size>k):
+                heapq.heappop(dist)
+                size-=1
+        ans = heapq.nsmallest(k,dist)
+        return [a[1] for a in ans]
+
+        
+
+        
