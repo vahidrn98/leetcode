cba0006e6745c12a14d7d2d4adb5ab34b627a7e0
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-09-25 12:02:12 -0230

    hard candy

--- Candy.py
+++ Candy.py
@@ -1,13 +1,49 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        inIsland = False
+        
         candies = [1]*len(ratings)
         for r in range(len(ratings)):
-            if((r>0 and ratings[r]>ratings[r-1]) ):
-                candies[r] = candies[r-1]+1
-            elif((r<len(ratings)-1 and ratings[r]>ratings[r+1])):
-                candies[r] = candies[r+1]+1
-
-            
-        return sum(candies)
+            _ = self.assign(ratings,r,candies)
         
+        return sum(candies)
+
+
+    def assign(self,ratings,i,candies):
+        if(i>=len(ratings) or i<0):
+            return 0
+        
+        if(candies[i]>1):
+            return candies[i]
+        
+        if(i<len(ratings)-1):
+            if(ratings[i]>ratings[i+1]):
+                candy_right = self.assign(ratings,i+1,candies)
+                if(i>0):
+                    if(ratings[i]>ratings[i-1]):
+                        candy_left = self.assign(ratings,i-1,candies)
+                        candies[i] = max(candy_left,candy_right)+1
+                        return candies[i]
+                    else:
+                        candies[i] = candy_right+1
+                        return candies[i]
+                else:
+                    candies[i] = candy_right+1
+                    return candies[i]
+            else:
+                if(i>0):
+                    if(ratings[i]>ratings[i-1]):
+                        candy = self.assign(ratings,i-1,candies)
+                        candies[i] = candy+1
+                        return candies[i]
+                    else:
+                        return candies[i]
+                else:
+                    return candies[i]
+
+        else:
+            if(ratings[i]>ratings[i-1]):
+                candy = self.assign(ratings,i-1,candies)
+                candies[i]=candy+1
+                return candies[i]
+            else:
+                return candies[i]
