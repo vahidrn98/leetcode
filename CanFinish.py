b7ec2433900b2f6378300efdb50654041f25f6d8
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-10-10 12:00:38 -0230

    canfinish update

--- CanFinish.py
+++ CanFinish.py
@@ -2,22 +2,34 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         cmap = {}
 
-        for x in prerequisites:
-            a,b = x
-            cmap[b] = a
+        for p in prerequisites:
+            if p[1] not in cmap:
+                cmap[p[1]] = [p[0]]
+            else:
+                cmap[p[1]].append(p[0])
 
         visited = {}
-
-
-        for i in range(numCourses):
-            if(i in cmap):
-                visited[i] = True
-        
-        for i in range(numCourses):
-            if(i in cmap and cmap[i] in visited):
+        for c in cmap:
+            if not self.dfs(c,cmap,visited):
                 return False
         
         return True
-
-
         
+    
+    def dfs(self,course, cmap, visited):
+
+        if(course in visited):
+            return False
+        
+        if(course not in cmap):
+
+            visited[course] = True
+            return True
+        
+        visited[course] = True
+        
+        for c in cmap[course]:
+            if not self.dfs(c,cmap,visited):
+                return False
+        
+        return True
