80bec6a1383e433c61ee1cd75fbd96047f6c2bf1
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-10-13 14:34:12 -0230

    medium wordBreak

--- WordBreak.py
+++ WordBreak.py
@@ -1,14 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wd = {x:i for i,x in enumerate(wordDict)}
-
-        return self.isInDict(s,wd)
+        segmented = {}
+        return self.isInDict(s,wd,segmented)
     
-    def isInDict(self,s,wordDict):
-
+    def isInDict(self,s,wordDict,segmented):
+        if(s in segmented):
+            return segmented[s]
         for i in range(len(s)):
             if(s[:i+1] in wordDict):
-                if(i+1 == len(s) or self.isInDict(s[i+1:],wordDict)):
+                if(i+1 == len(s) or self.isInDict(s[i+1:],wordDict,segmented)):
+                    segmented[s] = True
                     return True
-        
+        segmented[s] = False
         return False
