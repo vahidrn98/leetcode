361fcc825257e7e2571ad45b0d52381ebfce9229
Vahid Reza Niazmand <v1990mail@gmail.com>
2024-11-07 17:31:22 -0330

    medium longest consecutive

--- LongestConsecutive.py
+++ LongestConsecutive.py
@@ -1,18 +1,21 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        num_set = set(nums)
-        longest = 0
-        visited = {}
-        for n in nums:
-            if(n in visited):
-                continue
-            if n - 1 not in num_set:
-                length = 1
-
-                while n + length in num_set:
-                    visited[n+length] = True
-                    length += 1
-                
-                longest = max(longest, length)
+        nums.sort()
+        if not nums:
+            return 0
+        a = nums[0]
+        length = 1
+        maxLen = 1
+        if(len(nums)>1):
+            for n in nums[1:]:
+                if n==a+1:
+                    length +=1
+                    maxLen = max(maxLen,length)
+                elif(n>a+1):
+                    length=1
+                a = n
         
-        return longest
+        return maxLen
+
+
+        
